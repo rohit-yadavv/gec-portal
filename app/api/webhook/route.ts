c30e06f6b4c17950ae196f15eb2b19a2eb587edc
Rohit Yadav <rk4740779@gmail.com>
2024-02-13 20:27:08 +0530

    .

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -25,7 +25,7 @@ export async function POST(req: Request) {
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response('Error occured -- no svix headers', {
-      status: 400
+      status: 400 
     })
   }
  
