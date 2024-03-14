0aa5b35e273cd4c7a624c15907d80cd0d3201f14
Rohit Yadav <rk4740779@gmail.com>
2024-03-14 18:18:11 +0530

    updated loding of team route

--- app/(root)/team/loading.tsx
+++ app/(root)/team/loading.tsx
@@ -9,23 +9,25 @@ export const metadata: Metadata = {
 
 const Loading = () => {
   return (
-    <section>
+    <>
       <h1 className=" text-[30px] font-bold leading-[42px] tracking-tighter text-dark-100 dark:text-light-900">
-        The Team 
+        The Team
       </h1>
-      <div className="mb-12 mt-11 flex flex-wrap gap-5">
-        <Skeleton className="h-14 flex-1" />
-        <Skeleton className="h-14 w-28" />
-      </div>
-      <div className="flex flex-wrap gap-4">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
-          <Skeleton
-            key={item}
-            className="h-60 w-full rounded-2xl sm:w-[260px]"
-          />
-        ))}
-      </div>
-    </section>
+
+      <section className="mt-12 flex flex-col flex-wrap gap-6">
+        <div className="flex flex-wrap items-center justify-center gap-6">
+          <Skeleton className="h-60 w-full rounded-2xl sm:w-[260px]" />
+        </div>
+        <div className="flex flex-wrap items-center  justify-center gap-6">
+          {[1, 2, 3, 4, 5, 6].map((item) => (
+            <Skeleton
+              key={item}
+              className="h-60 w-full rounded-2xl sm:w-[260px]"
+            />
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
